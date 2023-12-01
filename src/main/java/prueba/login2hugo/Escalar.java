ed0bab67f764a4e1be40f6a200e01cbb6c9e78eb
hugo.marmor@educa.jcyl.es <Alumno@10.163.230.176>
2023-12-01 20:33:19 +0100

    Login copia seguridad

---
+++ src/main/java/prueba/login2hugo/Escalar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba.login2hugo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+/**
+ *
+ * @author Alumno
+ */
+public class Escalar {
+    public void escalarLabel(JLabel label, String rutaImagen) {
+        label.setIcon(new ImageIcon(new ImageIcon(getClass().getResource(rutaImagen)).getImage().getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_DEFAULT)));
+    }
+}
---
+++ src/main/java/prueba/login2hugo/Login2Hugo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prueba.login2hugo;
+
+/**
+ *
+ * @author HUGO MARTÍN MORALES DAM2B
+ */
+public class Login2Hugo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
